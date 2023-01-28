101c1c489c618afdb7769421242b20b586fac58a
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-01-28 19:12:37 +0530

    import awsmobile from "../aws-exports.js";

--- plugins/amplify.ts
+++ plugins/amplify.ts
@@ -1,11 +1,11 @@
 import { defineNuxtPlugin } from "#app";
 import { Amplify, Auth } from "aws-amplify";
-import awsExports from "../aws-exports.js";
+import awsmobile from "../aws-exports.js";
 
 // aws-exports should be dynamically generated
 
 export default defineNuxtPlugin((nuxtApp) => {
-  Amplify.configure(awsExports);
+  Amplify.configure(awsmobile);
 
   return {
     provide: {
