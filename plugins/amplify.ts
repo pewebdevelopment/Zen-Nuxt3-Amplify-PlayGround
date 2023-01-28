d6a415f0c4a8b89f50a9453b4a961f9045bc0ad6
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-01-28 17:33:49 +0530

    // aws-exports should be dynamically generated

--- plugins/amplify.ts
+++ plugins/amplify.ts
@@ -2,6 +2,8 @@ import { defineNuxtPlugin } from "#app";
 import { Amplify, Auth } from "aws-amplify";
 import awsExports from "@/aws-exports";
 
+// aws-exports should be dynamically generated
+
 export default defineNuxtPlugin((nuxtApp) => {
   Amplify.configure(awsExports);
 
