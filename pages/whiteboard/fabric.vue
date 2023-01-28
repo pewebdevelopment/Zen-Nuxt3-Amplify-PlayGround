1f181ea2a5c37abbeda3853b6ecedec8bb46458c
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-01-28 13:31:44 +0530

    Initial Commit

---
+++ pages/whiteboard/fabric.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout name="wbfabric">
+    <div class="mydiv"><whiteboard></whiteboard></div>
+  </NuxtLayout>
+</template>
+
+<script>
+import Whiteboard from "@/components/WBfabric/WBfabric.vue";
+export default {
+  components: {
+    whiteboard: Whiteboard,
+  },
+};
+</script>
