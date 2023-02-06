2e75d7544d962051662da67bdeac64114bf072bf
fedorg <fed0000r@gmail.com>
2023-02-06 02:06:00 +0000

    vc page init cometchat

---
+++ components/CometChat.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- {{ JSON.stringify(user) }} -->
+    <div v-if="init"><CometChatUI /></div>
+  </div>
+</template>
+<script setup lang="ts">
+import { CometChatUI } from "../cometchat-pro-vue-ui-kit/CometChatWorkspace/src/components/CometChatUI"
+import { CometChat } from '@cometchat-pro/chat';
+const user = ref()
+const init = ref(false)
+const props = defineProps<{
+  consts: { APP_ID: string, REGION: string, AUTH_KEY: string }
+  bar?: number
+}>()
+onMounted(() => {
+  const CONSTS = props.consts
+  // const CometChat = (await import('@cometchat-pro/chat')).CometChat;
+  let appSetting = new CometChat.AppSettingsBuilder()
+    .subscribePresenceForAllUsers()
+    .setRegion(CONSTS.REGION)
+    .autoEstablishSocketConnection(true)
+    .build();
+  CometChat.init(CONSTS.APP_ID, appSetting).then(
+    () => {
+      init.value = true;
+      let UID = "SUPERHERO1"
+      CometChat.login(UID, CONSTS.AUTH_KEY).then(
+        u => {
+          user.value = u;
+          console.log(u)
+          console.log("Login successful.")
+        }, error => {
+          console.log("Login failed with exception:", error);
+        }
+      );
+    }, error => {
+      console.log("error in init", error);
+    }
+  );
+})
+</script>
---
+++ pages/vc/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <CometChat :consts="CONSTS" />
+    </div>
+</template>
+<script setup lang="ts">
+
+import CometChat from '~~/components/CometChat.vue'
+const CONSTS = {
+    APP_ID: "231496f7c6ed7937",
+    REGION: "EU",
+    AUTH_KEY: "04c60b49108ffdac298fef518b334260b9c5e7f4"
+}
+</script>
